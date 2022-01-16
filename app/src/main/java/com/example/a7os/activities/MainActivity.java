19694d11488f71617d302abbc8ea873bebb3e053
Suswan114 <suswan.biswas@gmail.com>
2022-01-16 16:35:45 +0530

    fragments added

---
+++ app/src/main/java/com/example/a7os/activities/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.a7os.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.a7os.R;
+import com.example.a7os.fragments.DashboardFragment;
+import com.example.a7os.fragments.HistoryFragment;
+import com.example.a7os.fragments.LeaderboardFragment;
+import com.example.a7os.fragments.WalletFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom_navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new DashboardFragment()).commit();
+        bottom_navigation=findViewById(R.id.bottom_nav);
+        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selectedFragment =null;
+
+                switch (item.getItemId())
+                {
+                    case R.id.dashboard:
+                        selectedFragment=new DashboardFragment();
+                        break;
+                        //open fragement
+                    case R.id.wallet:
+                        selectedFragment=new WalletFragment();
+                        break;
+                        //open fragement
+                    case R.id.history:
+                        selectedFragment=new HistoryFragment();
+                        break;
+                        //open fragement
+                    case R.id.leaderboard:
+                        selectedFragment=new LeaderboardFragment();
+                        break;
+                        //open fragement
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
+            }
+        });
+        // check
+    }
+}
