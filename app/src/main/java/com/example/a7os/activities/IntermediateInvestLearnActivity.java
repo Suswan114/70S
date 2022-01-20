465fae711eb9ee46a100ec2c36d89cd760091dc8
Suswan114 <infinityplaycons04@gmail.com>
2022-01-20 22:04:43 +0530

    learn page updated

---
+++ app/src/main/java/com/example/a7os/activities/IntermediateInvestLearnActivity.java
@@ -0,0 +1,16 @@
+package com.example.a7os.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a7os.R;
+
+public class IntermediateInvestLearnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intermediate_invest_learn);
+    }
+}
--- app/src/main/java/com/example/a7os/fragments/LearnFragment.java
+++ app/src/main/java/com/example/a7os/fragments/LearnFragment.java
@@ -1,5 +1,7 @@
 package com.example.a7os.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +9,30 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.a7os.R;
+import com.example.a7os.activities.InvestmentLearnActivity;
 
 public class LearnFragment extends Fragment {
+
+    CardView savings,invest;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_learn,container,false);
+        View view= inflater.inflate(R.layout.fragment_learn,container,false);
+        invest=view.findViewById(R.id.invest);
+        invest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getActivity(), InvestmentLearnActivity.class);
+                startActivity(i);
+                ((Activity)getActivity()).overridePendingTransition(0,0);
+            }
+        });
+        return view;
         //check
     }
 }
