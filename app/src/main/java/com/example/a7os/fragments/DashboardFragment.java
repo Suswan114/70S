73ef1f596c4e0ce774a96a8e23f2ef7f62c953b6
Suswan114 <infinityplaycons04@gmail.com>
2022-01-20 21:25:47 +0530

    plus and minus buttons implemented

--- app/src/main/java/com/example/a7os/fragments/DashboardFragment.java
+++ app/src/main/java/com/example/a7os/fragments/DashboardFragment.java
@@ -1,11 +1,13 @@
 package com.example.a7os.fragments;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,12 +19,16 @@ import com.example.a7os.activities.PlannerActivity;
 public class DashboardFragment extends Fragment {
 
     ImageView pie_chart_button;
+    ImageView plus,minus;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_dashboard,container,false);
         pie_chart_button=view.findViewById(R.id.main_page_pie);
+        plus=view.findViewById(R.id.add_button);
+        minus=view.findViewById(R.id.minus_button);
+
         pie_chart_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +36,70 @@ public class DashboardFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddDialog();
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMinusDialog();
+                // minus dialog
+            }
+
+        });
         return view;
     }
+
+
+    private void showAddDialog(){
+        Dialog add_dialog=new Dialog(getActivity());
+        add_dialog.setContentView(R.layout.add_dialog_layout);
+        add_dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        TextView ok_button=add_dialog.findViewById(R.id.ok_button);
+        TextView cancel_button=add_dialog.findViewById(R.id.cancel_button);
+
+        ok_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add data to file
+                add_dialog.dismiss();
+            }
+        });
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add_dialog.dismiss();
+            }
+        });
+        add_dialog.show();
+
+    }
+    private void showMinusDialog(){
+        Dialog minus_dialog=new Dialog(getActivity());
+        minus_dialog.setContentView(R.layout.minus_dialog_layout);
+        minus_dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        TextView ok_button=minus_dialog.findViewById(R.id.ok_button);
+        TextView cancel_button=minus_dialog.findViewById(R.id.cancel_button);
+        ok_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add data to file
+                minus_dialog.dismiss();
+            }
+        });
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                minus_dialog.dismiss();
+            }
+        });
+        minus_dialog.show();
+    }
+
+
 }
